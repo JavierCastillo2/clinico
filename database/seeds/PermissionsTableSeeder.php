86837b067f87cb685fa13d45e9db07f2b571807e
Stiven Pineda <jspineda74@misena.edu.co>
2021-10-20 17:45:59 -0500

    Eliminacion rol secretary y vista perfil

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -117,58 +117,58 @@ class PermissionsTableSeeder extends Seeder
             'users.show',
             'users.edit'
         ]);
-        $secretary = Role::create(['name' => 'Secretary']);
-        $secretary->givePermissionTo([
-            'patient.appointment',
-            'patient.appointments',
-            'change_password',
-            'doctor.appointments',
-            'doctor.assign_specialty',
-            'doctor.schedule.assignment',
-            'doctor.schedule.assign',
-            'doctor.update_specialty',
-            'patient.edit_password',
-            'patient.edit_profile',
-            'home',
-            'patient.invoice',
-            'patient.index',
-            'back.patient.appointment',
-            'all.appointments',
-            'back.patient.appointments',
-            'back.patient.invoice',
-            'back.patient.store_appointment',
-            'back.patient.appointment.edit',
-            'back.patient.appointment.update',
-            'clinical.data.index',
-            'clinical.data.store',
-            'clinical.data.create',
-            'clinic.note.store',
-            'clinic.note.update',
-            'clinic.note.destroy',
-            'clinic.note.edit',
-            'back.patient.edit.invoice',
-            'back.patient.update.invoice',
-            'patient.prescriptions',
-            'specialties.index',
-            'specialties.store',
-            'specialties.create',
-            'specialties.update',
-            'specialties.destroy',
-            'specialties.show',
-            'specialties.edit',
-            'store.appointment',
-            'patient.update_password',
-            'patient.update_profile',
-            'user.edit_profile',
-            'user.update_profile',
-            'users.store',
-            'users.index',
-            'users.create',
-            'users.destroy',
-            'users.update',
-            'users.show',
-            'users.edit'
-        ]);
+//        $secretary = Role::create(['name' => 'Secretary']);
+//        $secretary->givePermissionTo([
+//            'patient.appointment',
+//            'patient.appointments',
+//            'change_password',
+//            'doctor.appointments',
+//            'doctor.assign_specialty',
+//            'doctor.schedule.assignment',
+//            'doctor.schedule.assign',
+//            'doctor.update_specialty',
+//            'patient.edit_password',
+//            'patient.edit_profile',
+//            'home',
+//            'patient.invoice',
+//            'patient.index',
+//            'back.patient.appointment',
+//            'all.appointments',
+//            'back.patient.appointments',
+//            'back.patient.invoice',
+//            'back.patient.store_appointment',
+//            'back.patient.appointment.edit',
+//            'back.patient.appointment.update',
+//            'clinical.data.index',
+//            'clinical.data.store',
+//            'clinical.data.create',
+//            'clinic.note.store',
+//            'clinic.note.update',
+//            'clinic.note.destroy',
+//            'clinic.note.edit',
+//            'back.patient.edit.invoice',
+//            'back.patient.update.invoice',
+//            'patient.prescriptions',
+//            'specialties.index',
+//            'specialties.store',
+//            'specialties.create',
+//            'specialties.update',
+//            'specialties.destroy',
+//            'specialties.show',
+//            'specialties.edit',
+//            'store.appointment',
+//            'patient.update_password',
+//            'patient.update_profile',
+//            'user.edit_profile',
+//            'user.update_profile',
+//            'users.store',
+//            'users.index',
+//            'users.create',
+//            'users.destroy',
+//            'users.update',
+//            'users.show',
+//            'users.edit'
+//        ]);
 
         $doctor = Role::create(['name' => 'Doctor']);
         $doctor->givePermissionTo([
@@ -237,17 +237,17 @@ class PermissionsTableSeeder extends Seeder
         ]);
         $user->assignRole('Admin');
         //User Secretary
-        $user = User::find(2); //admin
-        $user->profile()->create([
-            'birthdate'=> Carbon::now()
-        ]);
-        $profile = $user->profile;
-        $profile->image()->create([
-                'url' => '/image/avatar.png',
-        ]);
-        $user->assignRole('Secretary');
+//        $user = User::find(2); //admin
+//        $user->profile()->create([
+//            'birthdate'=> Carbon::now()
+//        ]);
+//        $profile = $user->profile;
+//        $profile->image()->create([
+//                'url' => '/image/avatar.png',
+//        ]);
+//        $user->assignRole('Secretary');
         //User Doctor
-        $user = User::find(3); //admin
+        $user = User::find(2); //admin
         $user->profile()->create([
             'birthdate'=> Carbon::now()
         ]);
@@ -257,7 +257,7 @@ class PermissionsTableSeeder extends Seeder
         ]);
         $user->assignRole('Doctor');
         //User Patient
-        $user = User::find(4); //admin
+        $user = User::find(3); //admin
         $user->profile()->create([
             'birthdate'=> Carbon::now()
         ]);
